5302988000799dfcae2c7c143d390dbd9d0c3ff5
pimby <baraka608@student.mmarau.ac.ke>
2021-10-07 13:13:38 +0300

    edited minimum values.js

--- assignment oct 4/MINIMUM VALUES/minumum values.js
+++ assignment oct 4/MINIMUM VALUES/minumum values.js
@@ -1,14 +1,39 @@
-//const Arr = []
-function min(input) {
-    if (toString.call(input) !== "[object Array]")  
-      return false;
- return Math.min.apply(null, input);
-   }
+// //const Arr = []
+// function min(input) {
+//     if (toString.call(input) !== "[object Array]")  
+//       return false;
+//  return Math.min.apply(null, input);
+//    }
 
-function max(input) {
-    if (toString.call(input) !== "[object Array]")  
-      return false;
- return Math.max.apply(null, input);
-   }
-console.log(max([12,34,56,1]));
-console.log(min([-12,-34,0,-56,-1]));
+// function max(input) {
+//     if (toString.call(input) !== "[object Array]")  
+//       return false;
+//  return Math.max.apply(null, input);
+//    }
+// console.log(max([12,34,56,1]));
+// console.log(min([-12,-34,0,-56,-1]));
+
+// var item = items[Math.floor(Math.random() * items.length)];
+
+//console.log(Math.min.apply(myArr)); gave me infinity
+
+//console.log(Math.min(...myArr));//gave me Nan
+const myArr = ["750", "s40", "Audi", 125, "mk", "i", 4 , -12,-34,0,-56,-1];
+
+let miniMum = Infinity
+
+for (let i = 0; i < myArr.length; i++) {
+
+  if (typeof(myArr[i]) === "number"){
+
+      if (miniMum>myArr[i]) {
+
+        miniMum = myArr[i];
+
+      }
+
+  }
+
+}
+
+console.log(miniMum);
